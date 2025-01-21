00d9ad3203ab0a96f40df93466285c6b5bfe239c
unknown <jayallenalinday@gmail.com>
2025-01-21 18:49:53 +0800

    create-student function

--- src/student/student.controller.ts
+++ src/student/student.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
+import { StudentService } from './student.service';
+import { Student } from './entities/student.entity';
 
-@Controller('student')
-export class StudentController {}
+@Controller('students')
+export class StudentController {
+  constructor(private readonly studentService: StudentService) {}
+
+  @Post()
+  async create(@Body() studentData: Partial<Student>) {
+    return this.studentService.create(studentData);
+  }
+}
--- src/student/student.service.ts
+++ src/student/student.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Student } from './entities/student.entity';
 
 @Injectable()
-export class StudentService {}
+export class StudentService {
+  constructor(
+    @InjectRepository(Student)
+    private readonly studentRepository: Repository<Student>,
+  ) {}
+  async create(studentData: Partial<Student>): Promise<Student> {
+    const student = this.studentRepository.create(studentData);
+    return this.studentRepository.save(student);
+  }
+}
